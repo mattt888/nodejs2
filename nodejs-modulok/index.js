259bd86d27befd0ed95265dc2f74b931b90881bc
CSMate <csiremate@gmail.com>
2024-02-09 14:14:08 +0100

    fs AppendFileSync

--- nodejs-modulok/index.js
+++ nodejs-modulok/index.js
@@ -1,18 +1,24 @@
 var http = require('http')
 require('dotenv').config()
 const port = process.env.PORT || 3000
+const fs = require('fs');
 
-http.createServer( (req,res) => {
+http.createServer( (req, res) => {
 
-    const fileToCheck = 'path/to/your/file.txt';
+    res.writeHead( 200, {'Content-Type': 'text/html; charset=utf-8'})
 
-    fs.access(fileToCheck, fs.constants.F_OK, (err) => {
-    if (err) {
-        console.error('A fájl nem létezik vagy nem elérhető.');
-    } else {
-        console.log('A fájl létezik és elérhető.');
-    }
-});
+    // Fájlhoz adás
+    var isoDateString = new Date().toISOString();
+    const dataToAppend = "\nEz szinkron bejegyzés, ekkor készült: " 
+        + isoDateString + ' |||| ' + Date.now().toString().slice(-4) +'\n' 
+        + console.log('Fájlhoz való hozzáfűzés dátuma:', Date.now().toString().slice(-4)); 
+        // Az adat, amit hozzá szeretnénk adni
 
+    fs.appendFileSync(__dirname+'/test.txt', dataToAppend)
+        res.write(`<h2>OK, jelenlegi dátum: ${isoDateString}</h2>`);
+        res.write(`<h2>OK, jelenlegi dátum: ${Date.now().toString().slice(-4)}</h2>`);
+        console.log('console.log,   Jelenlegi dátum: ' + Date.now().toString().slice(-4));
+        console.log('Vége a kódnak ***********');
+        res.end()
 })
-.listen( port )
+.listen(port)
