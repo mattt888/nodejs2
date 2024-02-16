61ba87575f518f29e80a4ea8682c5ee27222d200
CSMate <csiremate@gmail.com>
2024-02-16 13:42:12 +0100

    createWriteStream with appendStream

--- nodejs-modulok/index.js
+++ nodejs-modulok/index.js
@@ -35,21 +35,51 @@ const server = http.createServer( (req, res) => {
             console.log('Parsed Data:', parsedData);
 
             const datetime = new Date();
-            const now = datetime.toISOString().replace('Z', '').replace('T', ' ');
+            const now = datetime.toISOString().replace('Z', '').replace('T', ' ').slice(0,-2);
             // var isoDateString = new Date().toISOString();
 
             const dataToAppend = "\nA komment objektumként tárolva: " + JSON.stringify(parsedData) +
                                  '\nEkkor készült/timestamp: '+ now
             // Az adat, amit hozzá szeretnénk adni
 
-            fs.appendFile(__dirname+'/test.txt', dataToAppend, err => {
-                if (err) {
-                    console.error('Hiba történt a fájlhoz adás során.', err);
-                  } else {
-                        console.log('Az adatokat/kommenteket sikeresen hozzáadtuk a fájlhoz, Jelenlegi dátum: ', now);
-                        console.log('Vége a kódnak -----------------------');
-                  }
-            })
+            const filePath = 'C:/Users/Mate/Downloads/Rebuild 09 04/nodejs2/nodejs-modulok/test222.txt';
+
+            const appendStream = fs.createWriteStream(filePath, { flags: 'a' });
+
+            appendStream.write(dataToAppend);
+            appendStream.end();
+
+            ///////////////////////// működik 2, createWriteStream-mel:
+            // const writableStream = fs.createWriteStream(filePath, { encoding: 'utf8' });
+            // // Adatok írása az adatfolyamba
+            // writableStream.write(dataToAppend)
+            // // Írás befejezése
+            // writableStream.end();
+            // // Hiba kezelése
+            // writableStream.on('error', (err) => {
+            // console.error('Hiba történt az írás során:', err);
+            // });
+            // const timestamp = () => {
+            //     console.log('Az adatokat/kommenteket sikeresen hozzáadtuk a fájlhoz, Jelenlegi dátum: ', now);
+            //     console.log('Vége a kódnak -----------------------');
+            // }
+            // // Írás befejezése utáni esemény
+            // writableStream.on('finish', () => {
+            //     timestamp();
+            // });
+            /////////////////////////////
+
+
+            ///////////////////////////////////////// működik 1, appendFile-lal:
+            // fs.appendFile(__dirname+'/test.txt', dataToAppend, err => {
+            //     if (err) {
+            //         console.error('Hiba történt a fájlhoz adás során.', err);
+            //       } else {
+            //             console.log('Az adatokat/kommenteket sikeresen hozzáadtuk a fájlhoz, Jelenlegi dátum: ', now);
+            //             console.log('Vége a kódnak -----------------------');
+            //       }
+            // })
+            ////////////////////////////////////////////
           
             // Most már a szerver válaszolhat a feldolgozott adatokkal
             res.write(`<p>Név: ${parsedData.name} </p>`)
